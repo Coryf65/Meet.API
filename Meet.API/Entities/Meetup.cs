fe6097bd788bdf27f8cdb061d773e5a6e502bbd4
Cory Fabian <Coryf65@users.noreply.github.com>
2023-01-31 20:56:58 -0600

    Created the classes that represent the DB

---
+++ Meet.API/Entities/Meetup.cs
@@ -0,0 +1,12 @@
+﻿namespace Meet.API.Entities;
+
+public class Meetup
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Organizer { get; set; }
+	public DateTime Date { get; set; }
+	public bool IsPrivate { get; set; }
+	public virtual Location Location { get; set; }
+	public virtual List<Lecture> Lecture { get; set; }
+}
