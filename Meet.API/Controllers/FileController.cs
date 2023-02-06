31a05b1ab368f98d2d4540e4343755339b3b9987
Cory Fabian <Coryf65@users.noreply.github.com>
2023-02-06 16:02:44 -0600

    limiting which files are allowed to download

---
+++ Meet.API/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Meet.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class FileController : ControllerBase
+{
+	[HttpGet]
+	public ActionResult GetFile(string filename)
+	{
+		string rootPath = Directory.GetCurrentDirectory();
+
+		string fullFilePath = rootPath + "/PrivateFiles" + filename;
+
+		if (!System.IO.File.Exists(fullFilePath))
+			return NotFound($"a file with the name: '{filename}' was not found.");
+
+		byte[] file = System.IO.File.ReadAllBytes(fullFilePath);
+		FileExtensionContentTypeProvider fileProvider = new();
+
+		fileProvider.TryGetContentType(fullFilePath, out var contentType);
+
+		return File(fileContents: file, contentType: contentType, fileDownloadName: filename);
+	}
+}
