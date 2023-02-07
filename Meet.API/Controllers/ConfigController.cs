4bb7ed7943b8fd711a980f30bf9b1370e2891b18
Cory Fabian <Coryf65@users.noreply.github.com>
2023-02-07 13:23:41 -0600

    We can also create utility based Methods
    This will reload our config file without the need to re-compile or restart

---
+++ Meet.API/Controllers/ConfigController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meet.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConfigController : ControllerBase
+{
+	private readonly IConfiguration _config;
+	private readonly ILogger<ConfigController> _logger;
+
+	public ConfigController(IConfiguration configuration, ILogger<ConfigController> logger)
+	{
+		_config = configuration;
+		_logger = logger;
+	}
+
+	[HttpOptions("reload")]
+	public ActionResult Reload()
+	{
+		try
+		{
+			((IConfigurationRoot)_config).Reload();
+			return Ok();
+		}
+		catch (Exception error)
+		{
+			_logger.LogError("Failed to reload the Config file, 'Meet.API.Controllers.ConfigController.Reload()' error: { error }", error.Message);
+			return StatusCode(500, "Failed to reload the Configuration File.");
+		}
+	}
+}
